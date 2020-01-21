1f7c21a496ef0316cba3dc38ac390059dd23e157
jingli <jingli.claire@gmail.com>
2020-01-21 22:16:45 +1100

    Pass all tests

--- bottles.rb
+++ bottles.rb
@@ -1,19 +1,5 @@
-class Bottles
+class Bottles  
   def verse(num)
-    # if num == 2
-    #   "2 bottles of beer on the wall, 2 bottles of beer.\n" +
-    #   "Take one down and pass it around, 1 bottle of beer on the wall.\n"
-    # elsif num == 1
-    #   "1 bottle of beer on the wall, 1 bottle of beer.\n" +
-    #   "Take it down and pass it around, no more bottles of beer on the wall.\n"
-    # elsif num == 0
-    #   "No more bottles of beer on the wall, no more bottles of beer.\n" +
-    #   "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
-    # else
-    #   "#{num} bottles of beer on the wall, #{num} bottles of beer.\n" +
-    #   "Take one down and pass it around, #{num-1} bottles of beer on the wall.\n"
-    # end
-
     qty_one = num == 1 ? "bottle" : "bottles"
     qty_two = num != 2 ? "bottles" : "bottle"
     phase_one = num == 1 ? "it" : "one"
@@ -33,6 +19,20 @@ class Bottles
   end
 
   def verses(num1, num2)
+    num_array = (num2..num1).to_a.reverse
+    string_array = []
+    num_array.each do |num|
+      if num!= num2
+        string_array << verse(num) + "\n"
+      else
+        string_array << verse(num)
+      end
+    end
 
+    string_array.join()
+  end
+
+  def song
+    verses(99, 0)
   end
 end
